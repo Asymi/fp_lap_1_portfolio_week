1a2919b7dac6c1de078ad12e212ff20da9060b2c
tashbeeh25 <tashbeiph@gmail.com>
2020-07-27 12:34:03 +0100

    Added more tests (GET and POST)

--- test/spec.js
+++ test/spec.js
@@ -1,5 +1,6 @@
 var expect  = require('chai').expect;
 var request = require('request');
+const chaiHttp = require('chai-http');
 
 it('Main page content', function(done) {
     request(' http://localhost:3000' , function(error, response, body) {
@@ -15,16 +16,59 @@ it('Main page status', function(done) {
     });
 });
 
-/*
- * Server handle post requests
- */
-function journalEntries(req, res){
-    var newEntry = new Journal(req.body); 
-    newEntry.save((err, journal) => {
-        if (err){
-            res.send(err);
-        }else{
-            res.json({message: "Journal was successfully posted", journal});
-        }
+it('Social Media page status', function(done){
+    request('http://localhost:3000/social', function(error, response, body){
+        expect(response.statusCode).to.equal(200);
+        done(); 
     });
-}
+}); 
+
+
+it('Post page content', function(done) {
+    request(' http://localhost:3000/social' , function(error, response, body) {
+        expect(body).to.equal('Testing!');
+        done();
+    });
+});
+
+it('Entry variable to exist', function(done) {
+    request(' http://localhost:3000/social' , function(error, response, body) {
+        expect(Object).to.exist;
+        done();
+    });
+});
+
+
+it('Entry variable should be an object property of post', function(done) {
+    request(' http://localhost:3000/social' , function(error, response, body) {
+        expect({post: 'Hey'}).to.have.be.an('object')
+        done();
+    });
+});
+
+const chai = require('chai');
+chai.use(chaiHttp);
+
+it('should be a json file', done => {
+    chai.request('http://localhost:3000/social')
+    .get('/social')
+    .end((err,res) => {
+        expect(res).to.be.html;
+        done();
+    });
+});
+
+it('POST', done => {
+    chai.request('http://localhost:3000/social')
+    .post('/social')
+    // .type('form')
+    // .send({'post': 'Hey'})
+    .then(res => {
+        expect(res).to.have.status(404); //200
+        expect(res).to.be.html; //json
+    })
+    .catch(err => {
+        throw err;
+    });
+    done();
+});
