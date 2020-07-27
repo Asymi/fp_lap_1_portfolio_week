3d2a47bbdbd0876ed4c60870bc4ecbb9389cce51
tashbeeh25 <tashbeiph@gmail.com>
2020-07-27 12:35:24 +0100

    Modified the server to work with tests

--- Server/server.js
+++ Server/server.js
@@ -7,8 +7,26 @@ const e = require('express');
 app.use(bodyParser.text());
 app.use(cors());
 
+const entry = { 
+    post: 'Hey'
+}
 
 app.get('/', (req, res) => res.send('Hello, client!'))
+
+app.get('/social', (req, res) => {
+    res.send('Testing!')
+})
+
+// app.post('/social', (req, res) => {
+//     res.json(entry);
+// })
+
+app.post('/social', (req, res) => {
+    // res.send('Made a new entry')
+    const newPost = JSON.parse(req.body); 
+    entry.push(newPost.name);  
+    console.log(entry); 
+})
 
 // To do: Get the server running
 app.listen(port, () => console.log(`Express now now running from http://localhost:${port}!`))
